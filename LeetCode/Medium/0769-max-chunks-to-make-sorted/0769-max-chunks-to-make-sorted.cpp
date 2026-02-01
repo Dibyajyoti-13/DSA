770b5c345ec6df9b6a8b431920908c0c969ed6b7
Dibyajyoti Sarkar <91973605+Dibyajyoti-13@users.noreply.github.com>
2026-02-01 09:30:30 +0530

    Time: 0 ms (100%), Space: 9.2 MB (80.05%) - LeetHub

---
+++ LeetCode/Medium/0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int maxSeen = 0;
+        int chunks = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+            maxSeen = max(maxSeen, arr[i]);
+            if (maxSeen == i) {
+                chunks++;
+            }
+        }
+
+        return chunks;
+    }
+};
