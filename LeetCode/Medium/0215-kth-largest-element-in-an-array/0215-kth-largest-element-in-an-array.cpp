ce7a6089f8420a3cfed8b382ff06cd9ac570653f
Dibyajyoti Sarkar <91973605+Dibyajyoti-13@users.noreply.github.com>
2026-02-20 14:51:41 +0530

    Time: 43 ms (19.08%), Space: 61.6 MB (34.56%) - LeetHub

--- LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,36 +1,13 @@
 class Solution {
 public:
-    void threeWayPartition(vector<int>& nums, int left, int right,
-                           int& lt, int& gt) {
-        int pivot = nums[left];
-        lt = left;
-        gt = right;
-        int i = left;
-
-        while (i <= gt) {
-            if (nums[i] < pivot) {
-                swap(nums[i++], nums[lt++]);
-            } else if (nums[i] > pivot) {
-                swap(nums[i], nums[gt--]);
-            } else {
-                i++;
-            }
-        }
-    }
-
-    int quickSelect(vector<int>& nums, int left, int right, int k) {
-        if (left > right) return -1;
-
-        int lt, gt;
-        threeWayPartition(nums, left, right, lt, gt);
-
-        if (k >= lt && k <= gt) return nums[k];
-        else if (k < lt) return quickSelect(nums, left, lt - 1, k);
-        else return quickSelect(nums, gt + 1, right, k);
-    }
-
     int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        return quickSelect(nums, 0, n - 1, n - k);
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(int i : nums){
+            pq.push(i);
+            if(pq.size() > k) pq.pop();
+        }
+
+        return pq.top();
     }
 };
