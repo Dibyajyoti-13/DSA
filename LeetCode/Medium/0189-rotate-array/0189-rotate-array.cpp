e1f5c3f5b7b309697424c0ac41c27db6e20634a4
Dibyajyoti Sarkar <91973605+Dibyajyoti-13@users.noreply.github.com>
2026-03-07 20:31:07 +0530

    Time: 0 ms (100%), Space: 29.6 MB (67.82%) - LeetHub

--- LeetCode/Medium/0189-rotate-array/0189-rotate-array.cpp
+++ LeetCode/Medium/0189-rotate-array/0189-rotate-array.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n;
+        k = n - k % n;
 
         for(int i = 0; i < gcd(n, k); i++){
             int curr = i;
-            int prev = nums[i];
+            int val = nums[i];
 
-            do{
+            while(true){
                 int next = (curr + k) % n;
-                int temp = nums[next];
-                nums[next] = prev;
-                prev = temp;
+
+                if(next == i) break;
+
+                nums[curr] = nums[next];
                 curr = next;
-            }while(i != curr);
+            }
+            nums[curr] = val;
         }
     }
 };
