ffa6a20b933faa082b102ddce3b9ff3d33ae486f
Dibyajyoti Sarkar <91973605+Dibyajyoti-13@users.noreply.github.com>
2026-03-15 19:53:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Hard/0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ LeetCode/Hard/0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -4,24 +4,25 @@ public:
         int k = nums.size();
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
         vector<int> result = {-100000, 100000};
-
         int high = INT_MIN;
+
         for(int i = 0; i < k ; i++){
             pq.push({nums[i][0], i, 0});
             high = max(high, nums[i][0]);
         }
 
-        while(true){
+        while(!pq.empty()){
             auto top = pq.top();
-            int low = top[0];
+            pq.pop();
 
+            int low = top[0];
             if(high - low < result[1] - result[0]){
                 result[0] = low;
                 result[1] = high;
             }
 
             if(top[2] + 1 >= nums[top[1]].size()) break;
-            pq.pop();
+
             pq.push({nums[top[1]][top[2] + 1], top[1], top[2] + 1});
             high = max(nums[top[1]][top[2] + 1], high);
         }
