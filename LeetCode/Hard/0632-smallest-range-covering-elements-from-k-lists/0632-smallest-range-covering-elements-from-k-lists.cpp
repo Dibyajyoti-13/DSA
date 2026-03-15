f0ec2445f68970f38860424b42196f3ae9990202
Dibyajyoti Sarkar <91973605+Dibyajyoti-13@users.noreply.github.com>
2026-03-15 20:08:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Hard/0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ LeetCode/Hard/0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,31 +1,47 @@
 class Solution {
 public:
+    struct Node {
+        int val, list, idx;
+        Node(int v, int l, int i) : val(v), list(l), idx(i) {}
+    };
+
+    struct cmp {
+        bool operator()(const Node &a, const Node &b) const {
+            return a.val > b.val;
+        }
+    };
+
     vector<int> smallestRange(vector<vector<int>>& nums) {
         int k = nums.size();
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        priority_queue<Node, vector<Node>, cmp> pq;
+
         vector<int> result = {-100000, 100000};
         int high = INT_MIN;
 
-        for(int i = 0; i < k ; i++){
-            pq.push({nums[i][0], i, 0});
+        for(int i = 0; i < k; i++){
+            pq.emplace(nums[i][0], i, 0);
             high = max(high, nums[i][0]);
         }
 
         while(!pq.empty()){
-            auto top = pq.top();
+            Node top = pq.top();
             pq.pop();
 
-            int low = top[0];
+            int low = top.val;
+
             if(high - low < result[1] - result[0]){
-                result[0] = low;
-                result[1] = high;
+                result = {low, high};
             }
 
-            if(top[2] + 1 >= nums[top[1]].size()) break;
+            if(top.idx + 1 >= nums[top.list].size())
+                break;
 
-            pq.push({nums[top[1]][top[2] + 1], top[1], top[2] + 1});
-            high = max(nums[top[1]][top[2] + 1], high);
+            int nextVal = nums[top.list][top.idx + 1];
+
+            pq.emplace(nextVal, top.list, top.idx + 1);
+            high = max(high, nextVal);
         }
+
         return result;
     }
 };
