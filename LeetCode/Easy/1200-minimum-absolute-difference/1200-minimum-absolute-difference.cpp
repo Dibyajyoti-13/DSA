92eb29ac4f5a57eed3395c0ea43f48e930801d84
Dibyajyoti Sarkar <91973605+Dibyajyoti-13@users.noreply.github.com>
2026-06-01 17:08:17 +0530

    Time: 14 ms (82.54%), Space: 36.5 MB (99.75%) - LeetHub

--- LeetCode/Easy/1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ LeetCode/Easy/1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -6,11 +6,13 @@ public:
         int diff = INT_MAX;
 
         for(int i = 1; i < arr.size(); i++){
-            diff = min(diff, arr[i] - arr[i - 1]);
-        }
-
-        for(int i = 1; i < arr.size(); i++){
-            if(arr[i] - arr[i - 1] == diff) result.push_back({arr[i - 1], arr[i]});
+            if(arr[i] - arr[i - 1] < diff){
+                diff = arr[i] - arr[i - 1];
+                result.clear();
+                result.push_back({arr[i - 1], arr[i]});
+            }
+        
+            else if(arr[i] - arr[i - 1] == diff) result.push_back({arr[i - 1], arr[i]});
         }
         return result;
     }
