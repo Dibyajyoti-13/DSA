6b6c72b592bbd20cfedf7713f0056057b2f242cf
Dibyajyoti Sarkar <91973605+Dibyajyoti-13@users.noreply.github.com>
2026-06-05 15:31:43 +0530

    Time: 0 ms (100%), Space: 23.9 MB (94.43%) - LeetHub

--- LeetCode/Easy/0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ LeetCode/Easy/0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,12 +1,18 @@
 class NumArray {
 public:
-    vector<int> arr;
+    vector<int> prefix;
+
     NumArray(vector<int>& nums) {
-        arr = nums;
+        int n = nums.size();
+        prefix.resize(n + 1, 0);
+
+        for(int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
     }
-    
+
     int sumRange(int left, int right) {
-        return accumulate(arr.begin() + left, arr.begin() + right + 1, 0);
+        return prefix[right + 1] - prefix[left];
     }
 };
 
