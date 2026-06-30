1dc1161d68fa603bb1694e8715c20a08e7944507
Dibyajyoti Sarkar <91973605+Dibyajyoti-13@users.noreply.github.com>
2026-06-30 12:17:28 +0530

    Time: 0 ms (100%), Space: 43.2 MB (18.69%) - LeetHub

--- LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,23 +1,5 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if(haystack.length() < needle.length()) return -1;
-        
-        char[] hay = haystack.toCharArray();
-        char[] nee = needle.toCharArray();
-
-        for(int i = 0; i < hay.length; i++){
-            int j = 0;
-            while(j < needle.length() && (i + j) < hay.length){
-                if(hay[i + j] == nee[j++]) continue;
-                else{
-                    j = -1;
-                    break;
-                }
-            }
-            if(j == needle.length()) return i;
-        }
-        return -1;
-
-        // return haystack.indexOf(needle);
+        return haystack.indexOf(needle);
     }
 }
