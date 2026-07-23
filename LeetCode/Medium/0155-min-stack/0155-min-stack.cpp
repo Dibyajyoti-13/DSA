77748c1557861138ee47807ee95e2d916bdf0596
Dibyajyoti Sarkar <91973605+Dibyajyoti-13@users.noreply.github.com>
2026-07-23 05:11:11 +0000

    Time: 61 ms (41.3%), Space: 155 MB (5.03%) - LeetHub

--- LeetCode/Medium/0155-min-stack/0155-min-stack.cpp
+++ LeetCode/Medium/0155-min-stack/0155-min-stack.cpp
@@ -1,35 +1,46 @@
+struct Node{
+    int val;
+    int min;
+    Node* next;
+    Node(int v, int x) : val(v), min(x), next(nullptr) {}
+};
+
 class MinStack {
-    stack<int> mainStack;
-    stack<int> minStack;
 public:
+    Node* head;
     MinStack() {
-        
+        head = nullptr;
     }
     
-    void push(int val) {
-        mainStack.push(val);
-        if(!minStack.empty()) minStack.push(min(minStack.top(), val));
-        else minStack.push(val);
+    void push(int value) {
+        int x = value;
+        if(head != nullptr) x = min(head->min, x);
+        Node* temp = new Node(value, x);
+        temp->next = head;
+        head = temp;
     }
     
     void pop() {
-        mainStack.pop();
-        minStack.pop();
+        if(head == nullptr) return;
+
+        Node* temp = head;
+        head = head->next;
+        delete temp;
     }
-    
+
     int top() {
-        return mainStack.top();
+        return head ? head->val : -1;
     }
-    
+
     int getMin() {
-        return minStack.top();
+        return head ? head->min : -1;
     }
 };
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
- * obj->push(val);
+ * obj->push(value);
  * obj->pop();
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
